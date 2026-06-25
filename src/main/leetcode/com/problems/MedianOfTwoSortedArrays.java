eee836d1db5f6972358770bf2ffcfed49ead34d1
Damian Wojakowski <wojakowski.damian@gmail.com>
2026-06-25 12:29:20 +0200

    adding simple implementation

--- src/main/leetcode/com/problems/MedianOfTwoSortedArrays.java
+++ src/main/leetcode/com/problems/MedianOfTwoSortedArrays.java
@@ -1,15 +1,20 @@
 package leetcode.com.problems;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class MedianOfTwoSortedArrays {
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] result = IntStream.concat(
+                Arrays.stream(nums1),
+                Arrays.stream(nums2)
+        ).sorted().toArray();
 
-        int firstArrayLength = nums1.length;
-        int secondArrayLength = nums2.length;
-
-        int midIndex = (firstArrayLength + secondArrayLength) / 2;
-
-        return 0.0;
-    };
-
+        if (result.length % 2 == 0) {
+            return (double) (result[result.length / 2] + result[(result.length / 2) - 1]) / 2;
+        } else {
+            return result[result.length / 2];
+        }
+    }
 }
--- src/test/leetcode/com/problems/MedianOfTwoSortedArraysTest.java
+++ src/test/leetcode/com/problems/MedianOfTwoSortedArraysTest.java
@@ -6,11 +6,61 @@ import org.junit.jupiter.api.Test;
 class MedianOfTwoSortedArraysTest {
 
     @Test
-    void GivenTwoArrays_ReturnsMedian() {
-        int[] array1 = new int[]{1,2};
-        int[] array2 = new int[]{3};
+    void GivenTwoArraysWithOddNumberOfElements_ReturnsMedian() {
+        int[] array1 = new int[]{1, 3};
+        int[] array2 = new int[]{2};
 
         int expectedMedian = 2;
+
+        assertEquals(expectedMedian, MedianOfTwoSortedArrays.findMedianSortedArrays(array1, array2));
+    }
+
+    @Test
+    void GivenTwoArraysWithEvenNumberOfElements_ReturnsMedian() {
+        int[] array1 = new int[]{1, 2};
+        int[] array2 = new int[]{3, 4};
+
+        double expectedMedian = 2.5;
+
+        assertEquals(expectedMedian, MedianOfTwoSortedArrays.findMedianSortedArrays(array1, array2));
+    }
+
+    @Test
+    void GivenTwoArraysWithRepeatingValues_ReturnsMedian() {
+        int[] array1 = new int[]{2, 2, 2, 5, 5, 5};
+        int[] array2 = new int[]{2, 6, 7};
+
+        double expectedMedian = 5;
+
+        assertEquals(expectedMedian, MedianOfTwoSortedArrays.findMedianSortedArrays(array1, array2));
+    }
+
+    @Test
+    void GivenFirstEmptyArray_ReturnsMedian() {
+        int[] array1 = new int[]{};
+        int[] array2 = new int[]{1, 2, 3, 4};
+
+        double expectedMedian = 2.5;
+
+        assertEquals(expectedMedian, MedianOfTwoSortedArrays.findMedianSortedArrays(array1, array2));
+    }
+
+    @Test
+    void GivenSecondEmptyArray_ReturnsMedian() {
+        int[] array1 = new int[]{2};
+        int[] array2 = new int[]{};
+
+        double expectedMedian = 2;
+
+        assertEquals(expectedMedian, MedianOfTwoSortedArrays.findMedianSortedArrays(array1, array2));
+    }
+
+    @Test
+    void GivenArrayWithNegativeNumbers_ReturnsMedian() {
+        int[] array1 = new int[]{1, 2};
+        int[] array2 = new int[]{-1, 3};
+
+        double expectedMedian = 1.5;
 
         assertEquals(expectedMedian, MedianOfTwoSortedArrays.findMedianSortedArrays(array1, array2));
     }
